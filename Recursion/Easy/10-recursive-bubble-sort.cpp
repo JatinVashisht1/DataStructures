926e84190d60a6af1191967bebe15ff57ebcba2e
Jatin Vashisht <81457315+JatinVashisht1@users.noreply.github.com>
2021-11-20 14:25:02 +0530

    Add files via upload

---
+++ Recursion/Easy/10-recursive-bubble-sort.cpp
@@ -0,0 +1,46 @@
+
+/**
+ * @author Jatin Vashisht
+ * @brief  Program for recursive bubble sort
+ */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief  Function to perform bubble sort recursively
+ * @param  *arr: array which is to sort
+ * @param  n: size of array
+ * @retval None
+ */
+void recursiveBubbleSort(int *arr, int n)
+{
+    if (n == 0) /// Base condition of recursion
+    {
+        return;
+    }
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    recursiveBubbleSort(arr, n - 1);
+}
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int main()
+{
+    int arr[5] = {5, 4, 6, 2, 10};
+    recursiveBubbleSort(arr, 5);
+    printArray(arr, 5);
+    return 0;
+}
